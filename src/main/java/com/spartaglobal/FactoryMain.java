11d093cda05dad6400367e2d4c04ef0dbe6c3917
djucacupueran <donovanjucajc@gmail.com>
2022-02-05 21:08:19 +0000

    View class implemented

--- src/main/java/com/spartaglobal/FactoryMain.java
+++ src/main/java/com/spartaglobal/FactoryMain.java
@@ -1,10 +1,14 @@
 package com.spartaglobal;
 
 import com.spartaglobal.controller.SortingController;
+import com.spartaglobal.view.SortingView;
 
 public class FactoryMain {
     public static void main(String[] args) {
-        SortingController s = new SortingController();
-        System.out.println(s.sortArray("bubble"));
+        SortingController controller = new SortingController();
+        SortingView view  = new SortingView();
+        String desiredAlgorithm = view.getSortingAlgorithm();
+        String result = controller.sortArray(desiredAlgorithm);
+        view.displayResults(result);
     }
 }
---
+++ src/main/java/com/spartaglobal/view/SortingView.java
@@ -0,0 +1,16 @@
+package com.spartaglobal.view;
+
+import java.util.Scanner;
+
+public class SortingView {
+    public String getSortingAlgorithm(){
+        System.out.println("Enter bubble or merge");
+        Scanner scanner = new Scanner(System.in);
+        String desiredAlgorithm = scanner.next();
+        return desiredAlgorithm;
+    }
+
+    public void displayResults(String result) {
+        System.out.println(result);
+    }
+}
